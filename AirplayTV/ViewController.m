2cf006b12c6537574538cf9cbe5213124e3d612a
chedao <chedao@chedaodeMac-mini.local>
2015-08-24 13:59:41 +0800

    Initial Commit

---
+++ AirplayTV/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AirplayTV
+//
+//  Created by chedao on 15/8/24.
+//  Copyright (c) 2015年 chedao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AirplayTV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AirplayTV
+//
+//  Created by chedao on 15/8/24.
+//  Copyright (c) 2015年 chedao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
