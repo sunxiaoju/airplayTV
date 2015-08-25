e96e4501038904b2dcb894d9a6eebe16b502ce50
sunxiaoju <Sxj081129@163.com>
2015-08-25 10:08:34 +0800

    first commit

---
+++ AirplayTV/VideoView.h
@@ -0,0 +1,13 @@
+//
+//  VideoView.h
+//  AirplayTV
+//
+//  Created by chedao on 15/8/24.
+//  Copyright (c) 2015年 chedao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoView : UIView
+
+@end
---
+++ AirplayTV/VideoView.m
@@ -0,0 +1,21 @@
+//
+//  VideoView.m
+//  AirplayTV
+//
+//  Created by chedao on 15/8/24.
+//  Copyright (c) 2015年 chedao. All rights reserved.
+//
+
+#import "VideoView.h"
+
+@implementation VideoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
